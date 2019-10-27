6276d4ef68639083c6b2816ef45940149a0e5464
benhayehudi <bengreenberg@gmail.com>
2019-10-27 11:15:01 +0200

    add skeleton login/logout with github-omniauth and fix webpacker issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
+
+OmniAuth.config.allowed_request_methods = [:post, :get]
